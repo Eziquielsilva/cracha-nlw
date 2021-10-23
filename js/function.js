1f14ee4777e4e1eceffbec2d2fef72ab86831431
Eziquiel L Silva <ezickiel@gmail.com>
2021-10-23 20:38:45 -0300

    criando js

---
+++ js/function.js
@@ -0,0 +1,31 @@
+
+const linksRedesSocial = {
+  github: 'Eziquielsilva',
+  youtube: 'UCdZXAVlnyA32zr8XknC6Y2g',
+  facebook: 'eziquiel.els',
+  instagram: 'zick_silva',
+  twitter: 'zick300'
+}
+
+function changeSocialMedialinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksRedesSocial[social]}`
+  }
+}
+
+function getGithubProfile() {
+  const url = `https://api.github.com/users/${linksRedesSocial.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImg.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGithubProfile()
